052fe14865782e07da07e06be773a2c67e55b877
Kamil Szczepański <kamil.szczepanski@netguru.com>
2022-08-16 11:55:02 +0200

    Initial Commit

---
+++ SecretStorageApp/SecretStorageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretStorageAppApp.swift
+//  SecretStorageApp
+//
+//  Created by Kamil Szczepanski on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SecretStorageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
